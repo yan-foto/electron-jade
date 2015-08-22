8e15f20dae4a06403c9ba52288574b900796a800
jvhoven <wolferine@live.se>
2015-08-22 16:54:15 +0200

    Only intercept .jade files

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
 var app = require('app');
-var fs = require('fs');
 var path = require('path');
 var jade = require('jade');
 var extend = require('util')._extend;
 
-module.exports = function(jadeOptions, locals) {
+module.exports = function(jadeOptions) {
   app.on('ready', function() {
     var protocol = require('protocol');
     var options = extend({}, jadeOptions || {});
 
-    protocol.interceptBufferProtocol('file', function(request, callback) {
-      var file = request.url.substr(7);
-      var content = null;
+    protocol.interceptProtocol('file', function(request, callback) {
+        if(request) {
+          var file = request.url.substr(8);
+          if(file.endsWith('.jade')) {
+            var compiled = jade.compileFile(file, jadeOptions)();
 
-      // See if file actually exists
-      try {
-        content = fs.readFileSync(file, 'utf8');
-      } catch (e) {
-        // See here for error numbers:
-        // https://code.google.com/p/chromium/codesearch#chromium/src/net/base/net_error_list.h
-       if (e.code === 'ENOENT') {
-         // NET_ERROR(FILE_NOT_FOUND, -6)
-         callback(6);
-       }
-
-       // All other possible errors return a generic failure
-       // NET_ERROR(FAILED, -2)
-       callback(2);
-      }
-
-      if (path.extname(file) === '.jade') {
-        var compiled = jade.compileFile(file, jadeOptions)(locals);
-
-        callback({data: new Buffer(compiled), mimeType:'text/html'});
-      } else {
-        callback({data: new Buffer(content)});
-      }
-    }, function (error, scheme) {
-      if (!error) {
-        console.log('jade interceptor registered successfully');
-      } else {
-        console.error('Jade interceptor failed:', error);
-      }
+            return new protocol.RequestStringJob({
+              mimeType: 'text/html',
+              data: compiled
+            });
+          } else {
+            return null;
+          }
+        }
+    }, function() {
+        console.log("jade active")
     });
   });
 };
